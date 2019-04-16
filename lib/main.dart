e3976993c638a4d0fab7215209afd27162782850
snarksliveshere <snarksliveshere@gmail.com>
2019-04-16 21:07:28 +0300

    named routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:flutter/rendering.dart';
 
 import './pages/auth.dart';
+import './pages/products_admin.dart';
 
 void main() {
 //  debugPaintSizeEnabled = true;
@@ -20,7 +21,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple
       ),
-      home: AuthPage()
+      home: AuthPage(),
+      routes: {
+        '/admin': (BuildContext context) => ProductsAdmin()
+      },
     );
   }
 }
