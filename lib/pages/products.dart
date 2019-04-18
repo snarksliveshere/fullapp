6f7e46abb61be17ba581a4bc9700323f415a8fbd
snarksliveshere <snarksliveshere@gmail.com>
2019-04-18 17:16:22 +0300

    Icon button

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -29,6 +29,15 @@ class ProductsPage extends StatelessWidget {
       ),
       appBar: AppBar(
         title: Text('EasyList'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+
+            },
+          ),
+
+        ],
       ),
       body: ProductManager(this.products),
     );
