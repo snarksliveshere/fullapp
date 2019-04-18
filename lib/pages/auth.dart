0a9dbd72b8aa1fb8736edbd19fe415b3c65c99f7
snarksliveshere <snarksliveshere@gmail.com>
2019-04-18 17:02:51 +0300

    add background image and filled by color

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -14,70 +14,68 @@ class _AuthPageState extends State<AuthPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login')
-      ),
-      body: _authForm()
-    );
+    return Scaffold(appBar: AppBar(title: Text('Login')), body: _authForm());
   }
 
   Widget _authForm() {
     return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/background.jpg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop)
-        )
-      ),
-      padding: EdgeInsets.all(10.0),
-      child: ListView(
-        children: <Widget>[
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Enter e-mail',
-            ),
-            onChanged: (String value) {
-              setState(() {
-                this._emailValue = value;
-              });
-            },
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Enter your password',
-            ),
-            onChanged: (String value) {
-              setState(() {
-                this._passwordValue = value;
-              });
-            },
-          ),
-          SwitchListTile(
-              title: Text('Accept terms'),
-              value: _acceptTerms,
-              onChanged: (bool value) {
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/background.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.2), BlendMode.dstATop))),
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+            child: SingleChildScrollView(
+                child: Column(
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Enter e-mail',
+                filled: true,
+                fillColor: Colors.white
+              ),
+              onChanged: (String value) {
                 setState(() {
-                  this._acceptTerms = value;
+                  this._emailValue = value;
                 });
-              }
-          ),
-          SizedBox(height: 10.0),
-          RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              onPressed: () => {
-                debugPrint(this._emailValue),
-                debugPrint(this._passwordValue),
-                Navigator.pushReplacementNamed(context, '/products')
               },
-              child: Text('Login')
-          )
-        ],
-      )
-    );
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Enter your password',
+                filled: true,
+                fillColor: Colors.white
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  this._passwordValue = value;
+                });
+              },
+            ),
+            SwitchListTile(
+                title: Text('Accept terms'),
+                value: _acceptTerms,
+                onChanged: (bool value) {
+                  setState(() {
+                    this._acceptTerms = value;
+                  });
+                }),
+            SizedBox(height: 10.0),
+            RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                onPressed: () => {
+                      debugPrint(this._emailValue),
+                      debugPrint(this._passwordValue),
+                      Navigator.pushReplacementNamed(context, '/products')
+                    },
+                child: Text('Login'))
+          ],
+        ))));
   }
 }
