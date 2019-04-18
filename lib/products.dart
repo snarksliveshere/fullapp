f4deb9ea5d028f2fa46667903333208b8d328461
snarksliveshere <snarksliveshere@gmail.com>
2019-04-18 16:45:37 +0300

    flexible and flex property

--- lib/products.dart
+++ lib/products.dart
@@ -8,66 +8,61 @@ class Products extends StatelessWidget {
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
         child: Column(
-          children: <Widget>[
-            Image.asset(this.products[index]['image']),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
+      children: <Widget>[
+        Image.asset(this.products[index]['image']),
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  flex: 10,
+                  child: Text(
                     this.products[index]['title'],
                     style: TextStyle(
                         fontSize: 26.0,
                         fontWeight: FontWeight.bold,
-                        fontFamily: 'Oswald'
-                    ),
+                        fontFamily: 'Oswald'),
                   ),
-                  SizedBox(width: 8.0,),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Text(
-                      '\$${this.products[index]['price']}',
-                      style: TextStyle(
-                        color: Colors.white
-                      ),
-                    )
-                  )
-                ],
-            )),
-            DecoratedBox(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  style: BorderStyle.solid,
-                  width: 1.0
                 ),
-                borderRadius: BorderRadius.circular(6.0)
-              ),
-              child: Padding(
-                child: Text('Union Square, San Francisco'),
-                padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5)
-              )
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(
-                  child: Text(
-                    'details'
-                  ),
-                  onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + index.toString()
-                  ),
-                )
+                SizedBox(
+                  width: 8.0,
+                ),
+                Expanded(
+                  flex: 6,
+                    child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 2.5),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).accentColor,
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Text(
+                          '\$${this.products[index]['price']}',
+                          style: TextStyle(color: Colors.white),
+                        )))
               ],
+            )),
+        DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.grey, style: BorderStyle.solid, width: 1.0),
+                borderRadius: BorderRadius.circular(6.0)),
+            child: Padding(
+                child: Text('Union Square, San Francisco'),
+                padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5))),
+        ButtonBar(
+          alignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('details'),
+              onPressed: () => Navigator.pushNamed<bool>(
+                  context, '/product/' + index.toString()),
             )
           ],
-        ));
+        )
+      ],
+    ));
   }
 
   @override
