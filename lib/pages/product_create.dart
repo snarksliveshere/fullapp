d7edfc8890d9462a2fc26032bafd8d83c5429ff2
snarksliveshere <snarksliveshere@gmail.com>
2019-04-18 10:11:53 +0300

    v-modal by flutter

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
 
   @override
+  State createState() {
+    return _ProductCreatePageState();
+  }
+}
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return Center(
-                    child: Text('This is a modal')
-                  );
-                }
-            );
-          }
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              this.titleValue = value;
+            });
+          },
+        ),
+        Text(titleValue)
+      ],
     );
   }
 }
