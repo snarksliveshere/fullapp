c0e0e10544f76175be6e0a449406a70f1e7d46a7
snarksliveshere <snarksliveshere@gmail.com>
2019-04-19 11:42:13 +0300

    refactoring create form fields to the Map

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -12,9 +12,12 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String _titleValue;
-  String _descriptionValue;
-  double _priceValue;
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'description': null,
+    'price': null,
+    'image': 'assets/food.jpg'
+  };
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField() {
@@ -31,9 +34,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         }
       },
       onSaved: (String value) {
-        setState(() {
-          this._titleValue = value;
-        });
+          this._formData['title'] = value;
       },
     );
   }
@@ -50,9 +51,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       },
       maxLines: 4,
       onSaved: (String value) {
-        setState(() {
-          this._descriptionValue = value;
-        });
+          this._formData['description'] = value;
       },
     );
   }
@@ -69,9 +68,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         }
       },
       onSaved: (String value) {
-        setState(() {
-          this._priceValue = double.parse(value.replaceFirst(RegExp(r','), '.'));
-        });
+          this._formData['price'] = double.parse(value.replaceFirst(RegExp(r','), '.'));
       },
     );
   }
@@ -80,15 +77,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     if (!_formKey.currentState.validate()) {
       return;
     }
-
     _formKey.currentState.save();
-    final Map<String, dynamic> product = {
-      'title': this._titleValue,
-      'description': this._descriptionValue,
-      'price': this._priceValue,
-      'image': 'assets/food.jpg'
-    };
-    widget.addProduct(product);
+    widget.addProduct(_formData);
     Navigator.pushReplacementNamed(context, '/products');
   }
 
