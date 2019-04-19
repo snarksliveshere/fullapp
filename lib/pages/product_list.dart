0c2c5e50d1bfb450dbeae8eb493fd4e1cd67d97d
snarksliveshere <snarksliveshere@gmail.com>
2019-04-19 14:26:50 +0300

    add visually remove item by swipe

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -12,31 +12,39 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return Column(children: <Widget>[
-          ListTile(
-            leading: CircleAvatar(
-                backgroundImage: AssetImage(products[index]['image'])),
-            title: Text(products[index]['title']),
-            subtitle: Text('\$${products[index]['price'].toString()}'),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
+        return Dismissible(
+          // title at now
+          key: Key(products[index]['title']),
+          background: Container(
+            color: Colors.red,
+            child: Text('REMOVING....', textAlign: TextAlign.center,)
           ),
-          Divider()
-        ]);
+          child: Column(children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image'])),
+              title: Text(products[index]['title']),
+              subtitle: Text('\$${products[index]['price'].toString()}'),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ProductEditPage(
+                          product: products[index],
+                          updateProduct: updateProduct,
+                          productIndex: index,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            Divider()
+          ]),
+        );
       },
       itemCount: products.length,
     );
