7e9f10f131cca8ef882f980071249db9944c4ce8
snarksliveshere <snarksliveshere@gmail.com>
2019-04-28 13:40:56 +0300

    add tunnel to device camera and gallery

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
 
@@ -9,6 +11,55 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  void _getImage(BuildContext context, ImageSource source) {
+    ImagePicker.pickImage(
+        source: source,
+        maxWidth: 400.0
+    )
+    .then((File image) {
+      Navigator.pop(context);
+    })
+    ;
+  }
+
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 150.0,
+            padding: EdgeInsets.all(10.0),
+            child: ListView(
+              children: <Widget>[
+                Text('Pick an Image'),
+                SizedBox(height: 10.0,),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text(
+                      'Use Camera',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700
+                      ),
+                  ),
+                  onPressed: () {
+                    _getImage(context, ImageSource.camera);
+                  },
+                ),
+                SizedBox(height: 10.0,),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text('Use Gallery'),
+                  onPressed: () {
+                    _getImage(context, ImageSource.gallery);
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final Color buttonColor = Theme.of(context).accentColor;
@@ -20,7 +71,7 @@ class _ImageInputState extends State<ImageInput> {
             width: 2.0
           ),
           onPressed: () {
-
+            _openImagePicker(context);
           },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
